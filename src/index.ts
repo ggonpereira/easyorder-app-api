fa5c09168472d78a068ab4ea5c960f9dc8b9fc69
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-14 23:59:36 -0300

    ✨ feat: add  required packages and start express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+const app = express();
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running at http://localhost:${SERVER_PORT} 🔥`);
+});
