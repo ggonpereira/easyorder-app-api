7f4a0ddb9ff38fefcbcaad299deccd8431a53c1d
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-15 15:16:49 -0300

    👌 improve: add try catch to listCategories

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export const listCategories = async (req: Request, res: Response) => {
-  const categories = await Category.find();
+  try {
+    const categories = await Category.find();
 
-  res.json(categories);
+    res.json(categories);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 };
