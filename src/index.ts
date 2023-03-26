6d190f40605272b09c32d02d4385714b217559b1
Gabriel Pereira <ggonpereira@gmail.com>
2023-03-26 17:21:56 -0300

    🎉 feat: configure headers to pass CORS

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,21 @@ import { router } from './router';
 dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL || '';
+const ALLOW_ORIGIN_VALUES = process.env.ALLOW_ORIGIN_VALUES || '';
+const ALLOW_METHODS_VALUES = process.env.ALLOW_METHODS_VALUES || '';
+const ALLOW_HEADERS_VALUES = process.env.ALLOW_HEADERS_VALUES || '';
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
     const app = express();
     const SERVER_PORT = process.env.SERVER_PORT;
+
+    app.use((_, res) => {
+      res.setHeader('Access-Control-Allow-Origin', ALLOW_ORIGIN_VALUES);
+      res.setHeader('Access-Control-Allow-Methods', ALLOW_METHODS_VALUES);
+      res.setHeader('Access-Control-Allow-Headers', ALLOW_HEADERS_VALUES);
+    });
 
     app.use(
       '/uploads',
